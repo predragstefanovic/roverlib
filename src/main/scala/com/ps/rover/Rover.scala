414fbb730f21489684990cc59bb04337717a8b4f
pstefanovic <predrag.stefanovicps@gmail.com>
2022-01-28 22:17:01 +0100

    RL-03 Move rover in all directions

--- src/main/scala/com/ps/rover/Rover.scala
+++ src/main/scala/com/ps/rover/Rover.scala
@@ -1,32 +1,67 @@
 package com.ps.rover
 
-import akka.actor.typed.ActorRef
-import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
 
 object Rover {
 
-    sealed trait Orientation
-    case object North extends Orientation
-    case object East extends Orientation
-    case object South extends Orientation
-    case object West extends Orientation
+  sealed trait Message
+  final case class CommandWrapper(command: Command, replyTo: ActorRef[Position]) extends Message
+  final case class Position(x: Int, y: Int, orientation: Orientation) extends Message
 
-    final case class Position(x: Int, y: Int, orientation: Orientation)
+  sealed trait Command
+  sealed trait MoveCommand extends Command
+  case object Forward extends MoveCommand
+  case object Backward extends MoveCommand
+  case object Left extends MoveCommand
+  case object Right extends MoveCommand
+  final case class Initialize(position: Position) extends Command
 
-    sealed trait Command
-    final case class Initialize(position: Position, replyTo: ActorRef[Position]) extends Command
+  sealed trait Orientation(val left: Orientation, val right: Orientation)
+  case object North extends Orientation(West, East)
+  case object East extends Orientation(North, South)
+  case object South extends Orientation(East, West)
+  case object West extends Orientation(South, North)
 
-    def apply(): Behavior[Command] = uninitialized()
+  def apply(): Behavior[Message] = uninitialized()
 
-    private def uninitialized(): Behavior[Command] = {
-        Behaviors.receivePartial {
-            case (context, Initialize(position, replyTo)) =>
-                replyTo ! position
-                initialized(position)
-        }
+  private def uninitialized(): Behavior[Message] = {
+    Behaviors.receivePartial {
+      case (_, CommandWrapper(Initialize(position), replyTo)) =>
+        replyTo ! position
+        initialized(position)
     }
+  }
 
-    private def initialized(currentPosition: Position): Behavior[Command] = Behaviors.stopped
+  private def initialized(currP: Position): Behavior[Message] = {
+    Behaviors.receivePartial {
+      case (_, CommandWrapper(cmd: MoveCommand, replyTo)) =>
+        val newP = (cmd match {
+          case Forward => Position.stepForward
+          case Backward => Position.stepBackward
+          case Left => Position.rotateLeft
+          case Right => Position.rotateRight
+        })(currP)
 
+        replyTo ! newP
+        initialized(newP)
+    }
+  }
+
+  object Position {
+
+    def rotateLeft(p: Position): Position = p.copy(orientation = p.orientation.left)
+    def rotateRight(p: Position): Position = p.copy(orientation = p.orientation.right)
+    def stepForward(p: Position): Position = step(p, 1)
+    def stepBackward(p: Position): Position = step(p, -1)
+
+    private def step(p: Position, i: Int): Position = p.orientation match {
+      case North => Position(p.x, p.y + i, p.orientation)
+      case South => Position(p.x, p.y - i, p.orientation)
+      case East => Position(p.x + i, p.y, p.orientation)
+      case West => Position(p.x - i, p.y, p.orientation)
+    }
+  }
 }
+
+
--- src/test/scala/com/ps/rover/RoverSpec.scala
+++ src/test/scala/com/ps/rover/RoverSpec.scala
@@ -1,26 +1,47 @@
 package com.ps.rover
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import com.ps.rover.Rover.{East, Initialize, Position}
+import com.ps.rover.Rover.{Backward, Command, CommandWrapper, East, Forward, Initialize, Left, North, Position, Right, South}
+import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.wordspec.AnyWordSpecLike
 
-class RoverSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+class RoverSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike with TableDrivenPropertyChecks {
 
   "A Rover" must {
     "initialize and reply its Position" in {
       val probe = createTestProbe[Position]()
       val rover = spawn(Rover())
-      rover ! Initialize(Position(4, 2, East), probe.ref)
+      rover ! CommandWrapper(Initialize(Position(4, 2, East)), probe.ref)
       probe.expectMessage(Position(4, 2, East))
     }
 
     "allow only one initialization" in {
       val probe = createTestProbe[Position]()
       val rover = spawn(Rover())
-      rover ! Initialize(Position(4, 2, East), probe.ref)
+      rover ! CommandWrapper(Initialize(Position(4, 2, East)), probe.ref)
       probe.expectMessage(Position(4, 2, East))
-      rover ! Initialize(Position(4, 2, East), probe.ref)
+      rover ! CommandWrapper(Initialize(Position(4, 2, East)), probe.ref)
       probe.expectNoMessage()
+    }
+
+    "move in all directions" in {
+      val probe = createTestProbe[Position]()
+      val rover = spawn(Rover())
+      rover ! CommandWrapper(Initialize(Position(4, 2, East)), probe.ref)
+      probe.expectMessage(Position(4, 2, East))
+
+      val cases = Table(
+        ("cmd", "position"),
+        (Forward, Position(5, 2, East)),
+        (Backward, Position(4, 2, East)),
+        (Left, Position(4, 2, North)),
+        (Right, Position(4, 2, East)),
+      )
+
+      forAll(cases) { (cmd: Command, position: Position) =>
+        rover ! CommandWrapper(cmd, probe.ref)
+        probe.expectMessage(position)
+      }
     }
   }
 
